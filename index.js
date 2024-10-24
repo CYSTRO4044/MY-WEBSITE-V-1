f6b323147c65823536ec6075161e260e5a8048c1
CꜝꫝYSTROゞ俟 <Cystro4044@outlook.com>
2024-10-24 18:14:03 +0000

    CYSTRO 4044

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
-
 app.get("/", async function (req, res) {
   res.sendFile(path.join(__dirname, "My.html"));
 });
 
 app.listen(port, () => {
-  console.log(chalk.hex('#ffcc00')(`رينننن بورت: ${port}`));
+  console.log(chalk.hex('#ffcc00')(`Welcome to my website : ${port}`));
 });
 
 module.exports = app
